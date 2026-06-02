c50cf4adac84c4944076267aae5affd5b732c317
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-06-02 10:58:59 -0400

    Group tests into valid input category

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -1,12 +1,17 @@
 import { capitalize } from "./capitalize";
 import { test, expect, describe } from "@jest/globals";
 
-describe("Happy cases", () => {
+describe("Valid inputs", () => {
   test.each([
     ["a", "A"],
     ["b", "B"],
     ["A", "A"],
     ["it", "It"],
+    ["1st", "1st"],
+    ["2nd place", "2nd place"],
+    ["...and you'll never know", "...and you'll never know"],
+    ["   congratulations  ", "   congratulations  "],
+    ["   ", "   "],
   ])("capitalize(%p) => %p", (input, output) => {
     expect(capitalize(input)).toBe(output);
   });
@@ -17,18 +22,6 @@ describe("Falsy and nullish", () => {
     ["", ""],
     [undefined, ""],
     [null, ""],
-  ])("capitalize(%p) => %p", (input, output) => {
-    expect(capitalize(input)).toBe(output);
-  });
-});
-
-describe("Non-alpha first character", () => {
-  test.each([
-    ["1st", "1st"],
-    ["2nd place", "2nd place"],
-    ["...and you'll never know", "...and you'll never know"],
-    ["   congratulations  ", "   congratulations  "],
-    ["   ", "   "],
   ])("capitalize(%p) => %p", (input, output) => {
     expect(capitalize(input)).toBe(output);
   });
