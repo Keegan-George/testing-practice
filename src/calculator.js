fde0bcd33130c18017cad2ea63f4c34657712aab
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-06-03 12:14:52 -0400

    Remove IIFE and use simple object for calculator to match the project specs

--- src/calculator.js
+++ src/calculator.js
@@ -1,24 +1,22 @@
-const calculator = (() => {
-  function add(x = 0, y = 0) {
+const calculator = {
+  add(x = 0, y = 0) {
     return x + y;
-  }
+  },
 
-  function subtract(x = 0, y = 0) {
+  subtract(x = 0, y = 0) {
     return x - y;
-  }
+  },
 
-  function multiply(x = 0, y = 0) {
+  multiply(x = 0, y = 0) {
     return x * y;
-  }
+  },
 
-  function divide(x = 0, y = 1) {
+  divide(x = 0, y = 1) {
     if (y === 0) {
       throw new Error("Cannot divide by 0");
     }
     return x / y;
-  }
-
-  return { add, subtract, multiply, divide };
-})();
+  },
+};
 
 export { calculator };
