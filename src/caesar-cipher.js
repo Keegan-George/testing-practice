4f03b3741da70536b770e987be2fc05edb6846b8
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2026-06-04 13:22:12 -0400

    Refactor function to handle wrapping

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,6 +1,8 @@
 function caesarCipher(str, shift) {
   const charCode = str.charCodeAt();
-  const newCode = charCode + shift;
+  const normalizedCharCode = charCode - 97;
+  const shifted = (normalizedCharCode + shift) % 26;
+  const newCode = shifted + 97;
   return String.fromCharCode(newCode);
 }
 
--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -5,8 +5,19 @@ describe("Valid inputs", () => {
   test.each([
     ["a", 0, "a"],
     ["a", 1, "b"],
-    ["b", 1, "c"],
     ["a", 25, "z"],
+    ["a", 26, "a"],
+    ["a", 27, "b"],
+    ["a", 52, "a"],
+    ["a", 53, "b"],
+    ["m", 0, "m"],
+    ["m", 1, "n"],
+    ["m", 13, "z"],
+    ["m", 14, "a"],
+    ["m", 26, "m"],
+    ["z", 0, "z"],
+    ["z", 1, "a"],
+    ["z", 26, "z"],
   ])("caesarCipher(%p,%p) => %p", (str, shift, output) => {
     expect(caesarCipher(str, shift)).toBe(output);
   });
